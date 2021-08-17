04e9e67807b526540419fa6b50c9c25b784f24b3
kamilot322 <maksim-cirin@mail.ru>
2021-08-18 00:04:34 +0300

    Условия

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
 "use strict"; //всегда писать
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
 
-const a = prompt("Один из последних просмотренных фильмов?",""),
-      b = prompt("На сколько оцените его?",""),
-      c = prompt("Один из последних просмотренных фильмов?",""),
-      d = prompt("На сколько оцените его?","");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//Циклы
 
-console.log(personalMovieDB);
+//while(uslovie){};
+
+//do{}while(uslovie);
+
+//for(let i = A; i < N; i++){}
