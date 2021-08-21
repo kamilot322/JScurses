66225d0ebfffc36900901a20ef5a4977b7a44de0
kamilot322 <maksim-cirin@mail.ru>
2021-08-21 06:26:03 +0300

    Функции

--- js/script.js
+++ js/script.js
@@ -1,66 +1,69 @@
 "use strict"; //всегда писать
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+let num = 30;
+
+function showFirstMessage(text) {
+    console.log(text);
+    num = 20;
+    console.log(num);
+}
+
+showFirstMessage("Hello Wolrd!");
+console.log(num);
+
+//Не ошибка!
+console.log(calc(4,3));
+console.log(calc(2,5));
+console.log(calc(5,7));
+
+
+function calc(a, b){
+    return(a + b);
+}
+
+//Function declaration
+
+//Не ошибка! Т.к. функция может использоваться до и после создания
+console.log(calc(4,3));
+console.log(calc(2,5));
+console.log(calc(5,7));
+
+function ret(){
+    let num = 50;
+    /*
+    some code
+    some code
+    */
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+//Function exprassion
+
+//Ошибка т.к. функция ещё не была создана,а уже используется
+logger();
+
+//Function exprassion
+const logger = function(){
+    console.log("Hello!");
 };
+logger();
 
-// for(let i = 0;i < 2; i++){
-//   const a = prompt("Один из последних просмотренных фильмов?",""),
-//     b = prompt("На сколько оцените его?","");
-  
-//   if (a != null && b != null && a !='' && b != '' && a.length < 50 ){
-//     personalMovieDB.movies[a] = b;
-//     console.log("done");
-//   }else{
-//     console.log('error');
-//     i--;
-//   }
-// }
+//Стрелочные ф-ции
 
-let i = 0;
+//Упрощённый вариант
+const calculate = (a, b) => a + b; 
+/*
+"calculate" - название ф-ции
+"= (a, b) =>" - синтаксис стрелочной ф-ции
+"(a, b)" - аргументы ф-ции
+"=>" - говорит о выполнении кода
+*/
 
-// while(i < 2){
-//   const a = prompt("Один из последних просмотренных фильмов?",""),
-//     b = prompt("На сколько оцените его?","");
-  
-//   if (a != null && b != null && a !='' && b != '' && a.length < 50 ){
-//     personalMovieDB.movies[a] = b;
-//     console.log("done");
-//   }else{
-//     console.log('error');
-//     i--;
-//   }
-//   i++;
-// }
-
-do{
-  const a = prompt("Один из последних просмотренных фильмов?",""),
-  b = prompt("На сколько оцените его?","");
-
-if (a != null && b != null && a !='' && b != '' && a.length < 50 ){
-  personalMovieDB.movies[a] = b;
-  console.log("done");
-}else{
-  console.log('error');
-  i--;
-}
-i++;
-}
-while(i < 2);
-
-if(personalMovieDB.count < 10){
-  console.log("Просмотрено довольно мало фильмов");
-}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-  console.log("Вы классический зритель");
-}else if(personalMovieDB.count >=30){
-  console.log("Вы киноман");
-}else{
-  console.log("Произошла ошибка");
-}
-
-console.log(personalMovieDB);
+//Классический вариант
+const calculate2 = (a, b) => {
+    console.log("Calc2");
+    return a + b;
+};
