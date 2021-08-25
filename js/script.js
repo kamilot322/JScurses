dc82c60ec012f0e8d4f8b9187807e4ab511830e7
kamilot322 <maksim-cirin@mail.ru>
2021-08-25 21:37:05 +0300

    Practice 3

--- js/script.js
+++ js/script.js
@@ -1,28 +1,70 @@
 "use strict"; //всегда писать
 
-const str = "tESt";
-const arr = [1, 2, 4, 6,3];
+let numberOfFilms;
 
-//Кол-во символов
-// console.log(str.length);
-// console.log(arr.length);
+function start(){
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-//Нельзя менять опеределенный символ в строке,делается с помощью методов
-// console.log(str[2]);
+    while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+}
 
-//Регистры
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
+start();
 
-const fruit = "some fruit";
-//Если выводится "-1", то отсутсвует символ или часть строки
-console.log(fruit.indexOf("q"));
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const logg = "Hello world";
-//Вырезка подстроки с n до q, может принимать отрицательные значения, может принимать один аргумент
-console.log(logg.slice(6, 11));
-//Вырезка подстроки тоже самое только без отриц чисел, отриц. число = 0, можно задавать 1-ый аргумент > чем 2-ой
-console.log(logg.substring(6, 11));
-//Вырезка подстроки с n-ой позиции и q элементов
-console.log(logg.substr(6, 5));
 
+function rememberMyFilms () {
+for(let i = 0;i < 2; i++){
+  const a = prompt("Один из последних просмотренных фильмов?",""),
+    b = prompt("На сколько оцените его?","");
+  
+  if (a != null && b != null && a !='' && b != '' && a.length < 50 ){
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  }else{
+    console.log('error');
+    i--;
+  }
+}
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel () {
+    if(personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов");
+      }else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log("Вы классический зритель");
+      }else if(personalMovieDB.count >=30){
+        console.log("Вы киноман");
+      }else{
+        console.log("Произошла ошибка");
+      }
+}
+
+detectPersonalLevel();
+
+
+function showMyDB () {
+    if(personalMovieDB.privat == false){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres () {
+    for(let i = 1; i < 4; i++){
+        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+ writeYourGenres();
