b4826c6bc9df2dd43e746c0ab5bb971fc502ad97
kamilot322 <maksim-cirin@mail.ru>
2021-08-27 17:27:45 +0300

    Объекты, деструктуризация объектов (ES6)

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
 "use strict"; //всегда писать
 
-function first() {
-    //do something
-    setTimeout(function(){
-        console.log(1);
-    },500);
+// const obj = new object();
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        background: "red"
+    }
+};
+
+console.log(Object.keys(options).length); //Кол-во свойств в объекте
+
+// console.log(options['colors']['border']);
+
+// delete options.name;
+
+// console.log(options);
+
+//Счётчик свойств объекта
+let counter = 0;
+
+//Перебор свойств options
+for (let key in options){
+    //Свойства в объекте объекта
+    if (typeof(options[key])== 'object'){
+        for(let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++; //Вложенные свойства
+        }
+    } else{
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++; //Верхний уровень
+    }
 }
 
-function second(){
-    console.log(2);
-}
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
+//Классический метод счёта свойств в объекте
+for (let key in options){
+        counter++; //Верхний уровень 
 }
 
-function done(){
-    console.log("Я прошёл этот урок");
-}
-
-learnJS("Javascript", done);
-
+console.log(counter);
