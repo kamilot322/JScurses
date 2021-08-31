8cd9656c896df392288b7ef2ec7d5c853cb2715b
kamilot322 <maksim-cirin@mail.ru>
2021-09-01 00:02:55 +0300

    Массивы и псевдомассивы

--- js/script.js
+++ js/script.js
@@ -1,44 +1,43 @@
 "use strict"; //всегда писать
 
-// const obj = new object();
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: "black",
-        background: "red"
-    }
-};
+const arr = [0, 21, 33, 12, 66, 18];
+arr.sort(compareNum);
+console.log(arr);
 
-console.log(Object.keys(options).length); //Кол-во свойств в объекте
-
-// console.log(options['colors']['border']);
-
-// delete options.name;
-
-// console.log(options);
-
-//Счётчик свойств объекта
-let counter = 0;
-
-//Перебор свойств options
-for (let key in options){
-    //Свойства в объекте объекта
-    if (typeof(options[key])== 'object'){
-        for(let i in options[key]){
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-            counter++; //Вложенные свойства
-        }
-    } else{
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-        counter++; //Верхний уровень
-    }
+function compareNum(a, b){
+    return a - b;
 }
 
-//Классический метод счёта свойств в объекте
-for (let key in options){
-        counter++; //Верхний уровень 
+//Метод перебора массива с callback-функцией внутри item - элемент arr, i  -итератор,arr - ссылка на массив
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+
+// //Так не делать т.к. будет нарушаться "логика" массива
+// arr[99] = 0;
+// //Длина массива = последний " индекс + 1"
+console.log(arr.length);
+console.log(arr);
+
+
+//Удаление последнего элемента
+arr.pop();
+arr.push(10);
+
+console.log(arr);
+
+for (let i = 0;i < arr.length; i++){
+    console.log(arr[i]);
 }
 
-console.log(counter);
+for(let value of arr){
+    console.log(value);
+}
+
+const str = prompt("", "");
+//Разделение элементов массива где есть запятая
+const products = str.split(",");
+//Сортировка массива
+products.sort();
+//Объединение массива, элементы через ;
+console.log(products.join(';'));
