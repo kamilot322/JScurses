14071a91c7600f0c628c16955f75f8810df59606
kamilot322 <maksim-cirin@mail.ru>
2021-09-01 19:10:51 +0300

    Передача по ссылке или по значению, Spread оператор (ES6-ES9)

--- js/script.js
+++ js/script.js
@@ -1,43 +1,99 @@
 "use strict"; //всегда писать
 
-const arr = [0, 21, 33, 12, 66, 18];
-arr.sort(compareNum);
-console.log(arr);
+// let a = 5,
+//     b = a;
 
-function compareNum(a, b){
-    return a - b;
+// b = b + 5;
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; //Ссылка на obj
+// copy.a = 10;
+//Лог вывода: { a: 10, b: 1 }
+//            { a: 10, b: 1 } изменилось и в основном объекте
+// console.log(obj);
+// console.log(copy);
+
+//Поверхностная копия объектов, не делает копии со вложенными объектами
+// function copy(mainObj){
+//     let objCopy = {};
+
+//     let key;
+//     for(key in mainObj){
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+// }
+
+
+//Копирование объекта
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(numbers);
+// console.log(newNumbers);
+
+
+
+const numbers = {
+    a:2,
+    b:5,
+    c:{
+        x:7,
+        y:7
+    }
+};
+
+const add = {
+    d:7,
+    e:20
+};
+//Копирование объекта
+const clone = Object.assign({},add);
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+//Копирование старого массива
+const oldArray = ["a","b","c"];
+const newArray = oldArray.slice();
+
+newArray[1] = "abcs";
+console.log(oldArray);
+console.log(newArray);
+
+//Оператор разворота
+
+const video = ["youtube", "vimeo","rutube"],
+    blogs = ["wordpress","livejournal","blogger"],
+    internet = [...video, ...blogs, "vk","facebook"];
+console.log(internet);
+
+function log(a, b, c){
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-//Метод перебора массива с callback-функцией внутри item - элемент arr, i  -итератор,arr - ссылка на массив
-arr.forEach(function(item, i, arr) {
-    console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+const num = [2, 4, 6];
+//2
+// 4
+// 6
+log(...num);
 
-// //Так не делать т.к. будет нарушаться "логика" массива
-// arr[99] = 0;
-// //Длина массива = последний " индекс + 1"
-console.log(arr.length);
-console.log(arr);
+const array = ["a", "b"];
 
+const newAaray = [...array];
 
-//Удаление последнего элемента
-arr.pop();
-arr.push(10);
+const q = {
+    one: 1,
+    two: 2
+};
 
-console.log(arr);
-
-for (let i = 0;i < arr.length; i++){
-    console.log(arr[i]);
-}
-
-for(let value of arr){
-    console.log(value);
-}
-
-const str = prompt("", "");
-//Разделение элементов массива где есть запятая
-const products = str.split(",");
-//Сортировка массива
-products.sort();
-//Объединение массива, элементы через ;
-console.log(products.join(';'));
+const newObj = {...q};
